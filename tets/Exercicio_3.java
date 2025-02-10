1dfb36308df4d680d2de6dff69f4d72f459dca4a
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2025-02-10 14:25:53 +0000

    Salvar

---
+++ tets/Exercicio_3.java
@@ -0,0 +1,13 @@
+public class Exercicio_3 {
+    public static void main(String[] args) {
+        
+        for (int i = 100; i < 121; i++){
+            
+            if(i % 2 == 0){
+                
+                System.out.println("Valor de i: " + i);
+            }
+
+    }
+    
+}
