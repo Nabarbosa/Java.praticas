9f925e086be6501cd2ae21883b90fff3899b555f
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2025-02-18 13:28:23 +0000

    Salvador

---
+++ atividade_2/src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Maria ViLa", 36,
+        new Endereco("Salvador", "Vila Rui Barbosa", "11B"));
+
+        System.out.println(cliente1.toString());
+    }
+}
